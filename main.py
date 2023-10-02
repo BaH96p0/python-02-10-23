19e62577c13a0c84b97fd9e72e061b5fa6c85ebd
BaH96p0 <144206430+BaH96p0@users.noreply.github.com>
2023-10-02 15:17:42 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,173 @@
+
+#три числа
+#
+# x = int(input())
+#
+# print(x)
+# print(x+1)
+# print(x+2)
+
+
+# сумма трех чисел
+#
+# a = int(input())
+# b = int(input())
+# c = int(input())
+#
+# print(a+b+c)
+
+
+
+
+#куб
+#
+# x = int(input())
+#
+# print('Объем =',x**3)
+# print('Площадь полной поверхности = ',6*x**2)
+
+
+
+
+#значение функции
+#
+# a = int(input())
+# b = int(input())
+#
+# print(3*(a+b)**3+275*b**2-127*a-41)
+
+
+
+
+#след и пред
+#
+# x = int(input())
+#
+# print('Следующее за числом',x,'число:',x+1,'для числа',x,'предыдущее число:',x-1)
+
+
+
+
+#стоимость покупки
+#
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+#
+# print((a+b+c+d)*3)
+
+
+
+#арифметическая прогрессия
+#
+# a1 = int(input())
+# d = int(input())
+# n = int(input())
+#
+# print(a1+d*(n-1))
+
+
+
+
+#разделяй и властвуй
+#
+# x = int(input())
+#
+# print(x,x*2,x*3,x*4,x*5,sep='--')
+
+
+
+
+# мандарины
+#
+# a = int(input())
+# b = int(input())
+#
+#
+# print(b//a)
+# print(b%a)
+
+
+
+#сама неотвратимость
+#
+#x = int(input())
+#
+#print(x//2+x%2)
+
+
+
+
+#пересчет временного интервала
+#
+#x = int(input())
+#
+#print(x,'мин - это',x//60,'час',x%60,'минут')
+
+
+
+#трехзначное число
+#
+# x = int(input())
+#
+# print((x//100)+(x%100//10)+((x%10)//1))
+
+
+
+
+#пегвине
+#
+# x = int(input())
+#
+# print('    _~_    ' * x)
+# print('   (0 0)   ' * x)
+# print('  /  V  \  ' * x)
+# print(' /(  _  )\ ' * x)
+# print('   ^^ ^^   ' * x)
+
+
+
+
+#вторая справа цифра
+# x = int(input())
+#
+# print(x//10)
+
+
+
+#электронные часы
+#
+# n = int(input())
+# print(n % (60 * 24) // 60,n % 60)
+
+#0 в 1 и наоборот
+
+# n = int(input())
+# print((n+1)%2)
+
+
+
+
+#следующее четное число
+#
+# n = int(input())
+# print(n//2*2+2)
+
+
+
+# МКАД
+#
+# v = int(input())
+# t = int(input())
+#
+# print(v*t%109)
+
+
+#улитка
+#
+#h = int(input())
+#a = int(input())
+#b = int(input())
+#print((h - a) // (a - b) + 1)
+
